060b18a01535543b172766621c7306499baeec40
keshavp <keshav.p17@gmail.com>
2025-05-05 20:29:00 +0530

    action button

--- src/main/resources/static/js/pagejs/station.js
+++ src/main/resources/static/js/pagejs/station.js
@@ -18,11 +18,107 @@ $(document).on("click", "#purchaseAddAction", function(e){
 	 $('#uomSel').empty();
 	 $('#ponumber').empty();
 	 
-	 getStationTypeList();
-	 getUOMList();
-	 getWorkCentreList();
+	 getStationTypeList("add");
+	 getUOMList("add");
+	 getWorkCentreList("add");
+	 
+	 $("#add_po").modal("show");
 });
-			
+
+
+$(document).on("click", ".edit-button", function(e){
+
+
+		var editId = $(this).attr('id');
+		console.log("editId----",editId);
+
+ 		getStationTypeList("edit");
+	 	getUOMList("edit");
+	 	getWorkCentreList("edit");
+
+		$("#edit_po").modal("show");
+
+
+		$.ajax({
+		    type: 'GET',
+		    url: server_url + `station/get/${editId}`,
+		    enctype: 'application/json',
+		    headers: authHeader,
+		    processData: false,
+		    contentType: false,
+		    data: null,
+		    success: function (response) {
+		
+				console.log("------response ----------",response);
+		
+				var data = response.payload;
+		
+				console.log("------response data----------",data);
+		
+			}
+		})
+});
+
+
+$(document).on("click", "#editPurchaseData", function(e){
+
+
+ 		getStationTypeList("edit");
+	 	getUOMList("edit");
+	 	getWorkCentreList("edit");
+
+$("#edit_po").modal("show");
+
+	 if(ValidationForSelectBox("#poErrAdd","Station Type",$('#addStationType')))
+		 if(NotAllowedNullVal("#poErrAdd","Station Number ",$('#addStationNumber')))
+			 if(ValidationForSelectBox("#poErrAdd","UOM ",$('#addUom')))
+				{
+		 
+		 var dataVal = {
+		 
+				 name				: $('#addStationNumber').val(),
+				 stationtypeid 		: $('#addStationType').val(),
+				 uomid				: $('#addUom').val(),
+				 workcenterid		: $('#addWorkCentre').val(),
+
+			};
+				 
+			 
+				 
+		 	console.log("====data==dataVal===",dataVal);
+				 
+				 
+				 $.ajax({
+						
+					   type: 'POST',
+					   url: server_url+"station/add",  //from API add new data
+					   data : JSON.stringify(dataVal),
+			//		   processData: false,
+					   headers: authHeader,
+					   contentType: "application/json; charset=utf-8",
+   
+					   success: function(result) {
+   	
+						console.log("insert--Information result==="+result);
+						
+						if(result.status=="CREATED"){
+							
+							getStationList();
+							
+							$("#add_po").modal("hide");
+						// $('#myTbody').empty();
+							
+						}else if(result.result==false){
+							
+							window.location.href = "sessionOut";
+							
+						}
+					}
+			});
+		}
+});
+
+	
 			
 //get purchase order list
 function getStationList(){  
@@ -96,7 +192,7 @@ function getStationList(){
 
 			
 //get StationType list
-function getStationTypeList(){
+function getStationTypeList(divId){
 		
 	$.ajax({
 	    type: 'GET',
@@ -110,14 +206,14 @@ function getStationTypeList(){
 	
 			console.log("==========response=====",response)
 			
+				$("#"+divId+"StationType").empty();
+				$("#"+divId+"StationType").append('<option value=' + 0+ '>  - Select Station Type - </option>');
 								
-					$("#addStationType").append('<option value=' + 0+ '>  - Select Station Type - </option>');
-									
-					$.each(response.payload, function( index, value ){
-									
-					$("#addStationType").append('<option value="'+ value.id + '">'+ value.name+' </option>');
-					
-				    });
+				$.each(response.payload, function( index, value ){
+								
+				$("#"+divId+"StationType").append('<option value="'+ value.id + '">'+ value.name+' </option>');
+				
+			    });
 				
 			}	
 		});
@@ -126,7 +222,7 @@ function getStationTypeList(){
 			
 			
 //get StationType list
-function getUOMList(){
+function getUOMList(divId){
 		
 	$.ajax({
 	    type: 'GET',
@@ -140,12 +236,12 @@ function getUOMList(){
 	
 			console.log("==========response=====",response)
 			
-								
-					$("#addUom").append('<option value=' + 0+ '>  - Select uom - </option>');
+					$("#"+divId+"Uom").empty();			
+					$("#"+divId+"Uom").append('<option value=' + 0+ '>  - Select uom - </option>');
 									
 					$.each(response.payload, function( index, value ){
 									
-					$("#addUom").append('<option value="'+ value.id + '">'+ value.name+' </option>');
+					$("#"+divId+"Uom").append('<option value="'+ value.id + '">'+ value.name+' </option>');
 					
 				    });
 				
@@ -155,7 +251,7 @@ function getUOMList(){
 } //end of get StationType list			
 
 //get StationType list
-function getWorkCentreList(){
+function getWorkCentreList(divId){
 		
 	$.ajax({
 	    type: 'GET',
@@ -169,12 +265,12 @@ function getWorkCentreList(){
 	
 			console.log("==========response=====",response)
 			
-								
-					$("#addWorkCentre").append('<option value=' + 0+ '>  - Select workcenter - </option>');
+					$("#"+divId+"WorkCentre").empty();			
+					$("#"+divId+"WorkCentre").append('<option value=' + 0+ '>  - Select workcenter - </option>');
 									
 					$.each(response.payload, function( index, value ){
 									
-					$("#addWorkCentre").append('<option value="'+ value.id + '">'+ value.name+' </option>');
+					$("#"+divId+"WorkCentre").append('<option value="'+ value.id + '">'+ value.name+' </option>');
 					
 				    });
 				
@@ -212,7 +308,7 @@ $(document).on("click", "#addPurchaseData", function(e){
 					   type: 'POST',
 					   url: server_url+"station/add",  //from API add new data
 					   data : JSON.stringify(dataVal),
-					   processData: false,
+			//		   processData: false,
 					   headers: authHeader,
 					   contentType: "application/json; charset=utf-8",
    
@@ -222,7 +318,7 @@ $(document).on("click", "#addPurchaseData", function(e){
 						
 						if(result.status=="CREATED"){
 							
-							getPOList();
+							getStationList();
 							
 							$("#add_po").modal("hide");
 						// $('#myTbody').empty();
@@ -232,13 +328,12 @@ $(document).on("click", "#addPurchaseData", function(e){
 							window.location.href = "sessionOut";
 							
 						}
-						
-						
-		
-					   }
+					}
 			});
 		}
 });
+
+
 
 $(document).on("click", ".delete-button", function () {
 
@@ -257,8 +352,8 @@ buttons: [
     
         $.ajax({
             
-    });	
-}
+   	 	});	
+	}
 })
 });
 	
