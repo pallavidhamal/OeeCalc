553099833e7eb5deb1054cda42306a824f1071c5
pallavid <pallavi.dhamal@scubetechserv.com>
2025-07-15 13:10:01 +0530

    files

--- src/main/resources/static/js/pagejs/production.js
+++ src/main/resources/static/js/pagejs/production.js
@@ -3,17 +3,89 @@
 			var editProdID = "";
 			var tableData = $('#prodList').DataTable();
 			var editId;
+			var unitid;
 		
 			$(document).ready(function(){
 				
-				
+				getUnitList("add");	
 				console.log("========dataTableData=======",dataTableData);
 				
-			//	alert("HII");
+				
+				const today = new Date();
+				const year = today.getFullYear();
+				let month = today.getMonth() + 1;
+				let day = today.getDate();
+
+				month = month < 10 ? '0' + month : month;
+				day = day < 10 ? '0' + day : day;
+
+				const formattedToday = `${year}-${month}-${day}`;
+				
+				const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+				let day1=1;
+				
+				const firstDayOfMonth1 = `${year}-${month}-01`;
+				
+				console.log("firstDayOfMonth1-",firstDayOfMonth1);
+				console.log("formattedToday-",formattedToday);
+
+				
+				 $('#prodDatefrm').val(firstDayOfMonth1);		 
+				 
+				 
+				 
+				$('#prodDateto').val(formattedToday);
+				
+
+				
+				
+				$('#addUnit').on('change', function (e) {
+				    var optionSelected = $("option:selected", this);
+				     unitid = this.value;
+					getWorkCentreList("add");
+					getUnitShifts();	
+					getFilterProductionList();
+					
+					
+				});
+			
+				$('#addWorkCenter').on('change', function (e) {
+					   
+					 var optionSelected = $("option:selected", this);
+					 var wsid = this.value;
+					 getMachinesByWc(wsid);
+					 getFilterProductionList();
+						
+				});
+				
+				getOpertors();
 				
 				getPOList();
 				
-			
+				
+
+
+				$(document).on("change", "#prodDatefrm", function(e){
+						getFilterProductionList();
+				});
+
+				$(document).on("change", "#prodDateto", function(e){
+								getFilterProductionList();
+				});
+
+				$(document).on("change", "#addShift", function(e){
+					getFilterProductionList();
+				});
+				
+				
+				$(document).on("change", "#addSelMachine", function(e){
+					getFilterProductionList();
+				});
+
+				$(document).on("change", "#addOperator", function(e){
+					getFilterProductionList();
+				});
+				
 				
 					
 			});
@@ -221,6 +293,11 @@
 					    			buttons: ['excel', 'print'],
 								 	 destroy: true,
 				    				 data: data,
+									 
+									 "columnDefs": [{
+									         "targets": "_all",
+									         "defaultContent": ""
+									     }],
 
 									  columns: [
 									{ "data": "proddate" },
@@ -1225,180 +1302,348 @@ function generatecatCodeListCustWise(divid,customerId,prodId){
 	});
 	
 	
-	// Get Excel PO Saved List Function
-	function getExcelPOSavedList(data) {
-		
-		$('#TestTbl').DataTable({
-				dom: 'Blfrtip',   
-				buttons: ['excel', 'print'],
-				destroy: true,
-		    	data: data,
-		    	"initComplete": function(settings, json) {
-				
-				},
-		    	columns: [
-		    		{ "data"	: 	"PO_NumberIN" },
-		    		{ "data"	: 	"PO_DateIN" },
-		    		{ "data"	: 	"PO_EndDateIN" },
-		    		{ "data"	: 	"CustomerIdIN" },
-		    		{ "data"	: 	"LineIdIN" },
-		    		{ "data"	: 	"CatalogIdIN" },
-		    		{ "data"	: 	"DescriptionIN" },
-		    		{ "data"	: 	"ProductIdIN" },
-		    		{ "data"	: 	"RegionIdIN" },
-		    		{ "data"	: 	"PO_QtyIN" },
-		    		{ "data"	: 	"Balace_QtyIN" },
-		    		{ "data"	: 	"StatusIN" },
-		    	],
-		    	"columnDefs":[	
-				              
-				],
-		});
-		
-	}
-	
-	
-		// Get Excel PO Saved List Function
-	function getExcelPORemainingList(data) {
-		
-		$('#insertdatatbl').DataTable({
-				dom: 'Blfrtip',   
-				buttons: ['excel', 'print'],
-				destroy: true,
-		    	data: data,
-		    	"initComplete": function(settings, json) {
-				
-				},
-		    	columns: [
-		    		{ "data"	: 	"PO_Number" },
-		    		{ "data"	: 	"PO_Date" },
-		    		{ "data"	: 	"PO_EndDate" },
-		    		{ "data"	: 	"CustomerId" },
-		    		{ "data"	: 	"LineId" },
-		    		{ "data"	: 	"CatalogId" },
-		    		{ "data"	: 	"Description" },
-		    		{ "data"	: 	"ProductId" },
-		    		{ "data"	: 	"RegionId" },
-		    		{ "data"	: 	"PO_Qty" },
-		    		{ "data"	: 	"Balace_Qty" },
-		    		{ "data"	: 	"Status" },
-		    	],
-		    	"columnDefs":[	
-				              
-				],
-		});
-		
-	}
-	
-	$(document).on("click", "#savePurchaseData", function(e){
-		
-				var poDel="";
-		
-		if(temp!=null && temp!= "" &&  temp!= undefined){
-			
-			poDel = temp.replace(/,\s*$/, "");
-		}
-		
-		
-		
-		
-		
-		var myarray=[];
-		
-		var LineID ,Product, Description, Region, PQty;
-		
-		 var i =  0 ;
-		 
-		 if(validateEdtLineId("#poErredt"))
-			 if(validateEdtProduct())
-			 if(validateEdtRegion())
-			 if(validateEdtPOQty())
-			if(validateBalPOQty())
-		 {
-		 
-		 $("#myTbodyEdt").find('tr').each(function (){
-			
-			 PO_Id			=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(0).find('input').val();
-			 LineID			=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(1).find('input').val();
-			 Product		=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(2).find('select').val();
-			 Description	=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(3).find('select option:selected').text();
-			 Catlog			=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(4).find('select option:selected').text();
-			 Region			=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(5).find('select').val();
-			 PQty			=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(6).find('input').val();
-			 BQty			=  $("#myTbodyEdt").find('tr').eq(i).find('td').eq(7).find('input').val();
-			 
-			 console.log("------dataVal----LineID---		",LineID);
-			 console.log("------dataVal----Product---		",Product);
-			 console.log("------dataVal----Description---	",Description);
-			 console.log("------dataVal----Catlog---		",Catlog);
-			 console.log("------dataVal----Region---		",Region);
-			 console.log("------dataVal----PQty---			",PQty);
-			 console.log("------dataVal----BQty---			",BQty);
-			 i++; 
- 
-			 var lineData = {
-		 
-					 PO_Id			: PO_Id,
-					 lineid 		: LineID,
-					 ProductId 		: Product,
-					 description	: Description,
-					 catlog 		: Catlog,
-					 RegionId		: Region,
-					 POQty			: PQty,
-					 BalQty			:BQty
-			
-			};
- 
-			 myarray.push(lineData);
-			 
-			 console.log("====myarray==update====",myarray);
-			 
-			 
-			     
-			 
-			 var dataVal = {
-			 
-					 PO_NO			: $('#ponumberEdt').val(),
-					 Customer 		: $('#customerListEdt').val(),
-					 PODate			: $('#poDateEdt').val(),
-					 POEDate		: $('#poEndDateEdt').val(),
-					 PoDeleted   	: poDel,
-					 authKey		:localStorage.getItem("authkey"),
-					 userId		    :localStorage.getItem("userId"),
-					 lineData       : myarray
 
-				};
-			 console.log("====data==dataVal=update==",dataVal);
 	
-			 $.ajax({
+	
+	
+	
+	
+	function getUnitList(divId){
+			
+		$.ajax({
+		    type: 'GET',
+		    url: server_url + "unit/getActive",
+		    enctype: 'application/json',
+		    headers: authHeader,
+		    processData: false,
+		    contentType: false,
+		    data: null,
+		    success: function (response) {
 		
-						type: 'POST',
-					    url: url+"updatePO",  //from API update data
-					    data : JSON.stringify(dataVal),
-					    contentType: "application/json",
-	    
-					    success: function(result) {
-	    	
-					    console.log("Update--Information result=datadata=="+result);
-					    
-					    if(result.result==true){
-							
-							getPOList();
-							
-							$("#edit_po").modal("hide");
-							
-						}else if(result.result==false){
-							
-							window.location.href = "sessionOut";
-							
-						}
+				console.log("==========response=====",response)
+				
+						$("#"+divId+"Unit").empty();			
+						$("#"+divId+"Unit").append('<option value=' + 0+ '>  - Select Unit - </option>');
+										
+						$.each(response.payload, function( index, value ){
+										
+						$("#"+divId+"Unit").append('<option value="'+ value.id + '">'+ value.name+' </option>');
+						
+					    });
+					
+				}	
+			});
+		
+	}
+	
+	
+	function getWorkCentreList(divId){
+			
+		$.ajax({
+		    type: 'GET',
+		    url: server_url + "workcenter/getWorkcenterByUnit/"+unitid,
+		    enctype: 'application/json',
+		    headers: authHeader,
+		    processData: false,
+		    contentType: false,
+		    data: null,
+		    success: function (response) {
+		
+				console.log("==========response=====",response)
+				
+						$("#"+divId+"WorkCenter").empty();			
+						$("#"+divId+"WorkCenter").append('<option value=' + 0+ '>  - Select workcenter - </option>');
+										
+						$.each(response.payload, function( index, value ){
+										
+						$("#"+divId+"WorkCenter").append('<option value="'+ value.id + '">'+ value.name+' </option>');
+						
+					    });
+					
+				}	
+			});
+		
+	}
+	
+	function getUnitShifts()
+	{
+				
+					console.log("=====getUnitShifts====unitid===",unitid)
+					
+					
+						$.ajax({
+						       type: "GET",
+						       url: server_url + `shift/getShiftByUnit/`+unitid,
+						       enctype: "application/json",
+						       headers: authHeader,
+						       processData: false,
+						       contentType: false,
+						       data: null,
+						       success: function (response) {
+									$("#addShift").empty();
+								//	$("#editItem").empty();
+								//	$(".addShift").append('<option value="0">  Select item </option>');
+									
+									
+									shiftOptions='<option value="0">  Select Shift </option>';
+									
+									console.log("=====getUnitShifts=======",response)
+									
+									
+								//	$("#editItem").append('<option value="0">  Select item </option>');
+
+						           for (i = 0; i < response.payload.length; ++i) {
+						            //   $(".addShift").append(`<option value="${response.payload[i].shiftid}">${response.payload[i].name}</option>`);
+									
+									
+									shiftOptions=shiftOptions+`<option value="${response.payload[i].shiftid}">${response.payload[i].name}</option>`;
+									   
+									//   $("#editItem").append(`<option value="${response.payload[i].itemid}">${response.payload[i].itemdesc}</option>`);
+									   
+						           }
+								   
+								   $("#addShift").append(shiftOptions);
+								   
+						       },
+
+						       error: function (error) {
+						           /*console.log(error);
+						      		 if (error.status == 401) {
+							    	  window.location.href =  contextPath;
+							      } else {
+							    	if( error.responseJSON != undefined){
+										errmssge=error.responseJSON.status;	
+									
+										if (error.responseJSON.status=="500"){
+											console.log("in errr");
+											 errorBlock("#error_block", error.responseJSON.message);
+										}else{
+													 errorBlock("#error_block", error.responseJSON.errors.message);
+													}
+									}
+									else{
+							   	  		console.log("Server Error! Please contact administrator");
+							   	  		errorBlock("#error_block", "Server Error! Please contact administrator");
+							     	}
+						     	}*/
+						       },
+						   });
+			}
+			
+
+			function	getMachinesByWc(wsid)
+				{
+					
+					
+					$.ajax({
+							       type: "GET",
+							       url: server_url + `station/getStationByWc/`+wsid,
+							       enctype: "application/json",
+							       headers: authHeader,
+							       processData: false,
+							       contentType: false,
+							       data: null,
+							       success: function (response) {
+										$("#addSelMachine").empty();
+										//$("#editStation").empty();
+										
+										machinesOptions='<option value="0">  Select Station </option>';
+									//	$("#editStation").append('<option value="0">  Select station </option>');
+							           for (i = 0; i < response.payload.length; ++i) {
+										
+										
+										machinesOptions=machinesOptions+`<option value="${response.payload[i].id}">${response.payload[i].name}</option>`;
+										
+							           //   $(".addStation").append(`<option value="${response.payload[i].id}">${response.payload[i].name}</option>`);
+										 //  $("#editStation").append(`<option value="${response.payload[i].id}">${response.payload[i].name}</option>`);
+							           }
+									   
+									   $("#addSelMachine").append(machinesOptions);
+									   
+							       },
+
+							       error: function (error) {
+							           /*console.log(error);
+							      		 if (error.status == 401) {
+								    	  window.location.href =  contextPath;
+								      } else {
+								    	if( error.responseJSON != undefined){
+											errmssge=error.responseJSON.status;	
+										
+											if (error.responseJSON.status=="500"){
+												console.log("in errr");
+												 errorBlock("#error_block", error.responseJSON.message);
+											}else{
+														 errorBlock("#error_block", error.responseJSON.errors.message);
+														}
+										}
+										else{
+								   	  		console.log("Server Error! Please contact administrator");
+								   	  		errorBlock("#error_block", "Server Error! Please contact administrator");
+								     	}
+							     	}*/
+							       },
+							   });
+				}			
+
+				function getOpertors()
+				{
+					
+					
+					console.log("oper");
+					$.ajax({
+								       type: "GET",
+								       url: server_url + `operator/allActive`,
+								       enctype: "application/json",
+								       headers: authHeader,
+								       processData: false,
+								       contentType: false,
+								       data: null,
+								       success: function (response) {
+											$("#addOperator").empty();
+										//	$("#editItem").empty();
+											//$(".addItem").append('<option value="0">  Select item </option>');
+											
+											itemOptions='<option value="0">  Select Operator </option>';
+
+											
+										//	$("#editItem").append('<option value="0">  Select item </option>');
+
+								           for (i = 0; i < response.payload.length; ++i) {
+								             
+											//  $(".addItem").append(`<option value="${response.payload[i].itemid}">${response.payload[i].itemdesc}</option>`);
+											   
+											itemOptions=itemOptions+`<option value="${response.payload[i].id}">${response.payload[i].name}</option>`;
+											
+											//   $("#editItem").append(`<option value="${response.payload[i].itemid}">${response.payload[i].itemdesc}</option>`);
+								           }
+										   
+										   $("#addOperator").append(itemOptions);
+										   
+								       },
+
+								       error: function (error) {
+								           /*console.log(error);
+								      		 if (error.status == 401) {
+									    	  window.location.href =  contextPath;
+									      } else {
+									    	if( error.responseJSON != undefined){
+												errmssge=error.responseJSON.status;	
+											
+												if (error.responseJSON.status=="500"){
+													console.log("in errr");
+													 errorBlock("#error_block", error.responseJSON.message);
+												}else{
+															 errorBlock("#error_block", error.responseJSON.errors.message);
+															}
+											}
+											else{
+									   	  		console.log("Server Error! Please contact administrator");
+									   	  		errorBlock("#error_block", "Server Error! Please contact administrator");
+									     	}
+								     	}*/
+								       },
+								   });
+				}
+				
+				
+				function getFilterProductionList(){  
+					
+					var dataVal = 
+					{
+						 unitid       	: $('#addUnit').val(),
+						 workcenterid 	: $('#addWorkCenter').val(),
+						 shiftid       	: $('#addShift').val(),
+						 stationId 		: $('#addSelMachine').val(),
+						 operatorid     : $('#addOperator').val(),						 
+						 fromdate		: $('#prodDatefrm').val(),						 
+						 todate			: $('#prodDateto').val(),
+						 
+					};
+								
+					console.log("-------------------Welcome to product getFilterProductionList---------",dataVal);
+					$.ajax({
+					    type: 'POST',
+					    url: server_url + "production/getFilterProductions",
+					    enctype: 'application/json',
+					    headers: authHeader,
+					    processData: false,
+					    contentType: "application/json; charset=utf-8",
+					    data: JSON.stringify(dataVal),
+						
+						success: function (response) {		
+
+						console.log("------response data----------",response);
+
+						var data = response.payload;
+						console.log("------getPOList data----------",data);
+				//		console.log("------getPOList data.result----------",data.result);
+					
+						
+						tableData.destroy();
+			        $('#prodList.tbody').empty();
+
+					console.log("------111----------");
+
+					
+					
+			        //if(data.result == "success"){
+						
+			        /*var editIcon = function ( data, type, row ) 
+			        {
+					    if ( type === 'display' ) {
+					           
+					   	 	return '<span class="button" data-toggle="modal" data-target="#edit_po"> Edit </span>';
 					    }
-				});
-			 
-			 
-		 });		
-		 
-		 }//validation
-	});
-	
-	
+				       
+				    	return data;
+				    };*/
+					console.log("------222----------");
+
+			    
+			    	tableData = $('#prodList').DataTable( {
+				
+			    			dom: 'Blfrtip',   
+			    			buttons: ['excel', 'print'],
+						 	 destroy: true,
+		    				 data: data,
+							 
+							 "columnDefs": [{
+							         "targets": "_all",
+							         "defaultContent": ""
+							     }],
+
+							  columns: [
+							{ "data": "proddate" },
+						    { "data": "unitname" },
+						    { "data": "workcentername" },
+						    { "data": "shiftname" },
+							{ "data": "operatorname" },
+						    { "data": "stationname" },
+							
+						    { "data": "availabilityper" },
+						    { "data": "productivityper" },
+							{ "data": "rejectionper" },
+							{ "data": "oeeper" },
+							
+							 { "data":  null,
+					           render: function (data, type, row) {
+					               var id = data.id;
+					               var action = `<a  class="edit-button" id=${id}>View</a>`;
+					               return action;
+					           },
+				             },
+							
+				            ],
+				            "order": [[0, 'desc']],
+			    			} );
+							
+							
+							console.log("------333----------");
+						
+						
+						}
+					})
+				//		});
+				}												
+			
